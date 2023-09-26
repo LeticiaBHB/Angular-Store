0a8142816f61b81bcb21e5806eddae3a84705711
bowie <leticia.bini@gmail.com>
2023-09-26 14:21:49 -0300

    commit

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: ['./menu-bar.component.css', './menu-bar.responsive.component.css']
 })
 export class MenuBarComponent implements OnInit {
+  @ViewChild('menuToggle', { static: true }) menuToggle!: ElementRef;
+  @ViewChild('menuItems', { static: true }) menuItems!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    // Use o Renderer2 para adicionar a lógica do menu hamburger
+    this.renderer.listen(this.menuToggle.nativeElement, 'click', () => {
+      this.menuItems.nativeElement.classList.toggle('active');
+    });
   }
-
 }
